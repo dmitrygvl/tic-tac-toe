e24362997c202c03bb5073c38dd3f723ce1b9e5d
dmitrygvl <dmitrygv1994@ya.ru>
2024-02-07 01:42:49 +0300

    Fix socketManager

--- client/src/WebSocket/socketManager.ts
+++ client/src/WebSocket/socketManager.ts
@@ -1,5 +1,6 @@
 import WSManager from './WSManager';
-import pkg from '../../../package.json';
+// import pkg from '../../../package.json';
 import store from '../store/store';
 
-export const socketManager = new WSManager(pkg.proxy, store);
+// export const socketManager = new WSManager(pkg.proxy, store);
+export const socketManager = new WSManager(window.location.hostname, store);
